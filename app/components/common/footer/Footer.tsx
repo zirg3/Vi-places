b5920ded783ad157261b669d6807ce47dfca83af
zirg3 <zirg31@gamil.com>
2022-08-12 21:58:15 +0300

    create footer

---
+++ app/components/common/footer/Footer.tsx
@@ -0,0 +1,43 @@
+import styles from './Footer.module.scss'
+import {useRouter} from "next/router";
+import React from "react";
+import Link from "next/link";
+
+const navItems = [
+    {
+        icon: 'home',
+        link: '/'
+    },
+    {
+        icon: 'explore',
+        link: '/explore'
+    },
+    {
+        icon: 'place',
+        link: '/place'
+    },
+    {
+        icon: 'person_outline',
+        link: '/profile'
+    }
+]
+
+const Footer = () => {
+    const router = useRouter()
+
+    return (
+        <footer className={styles.footer}>
+            <nav>
+                {navItems.map(item => (
+                    <Link href={item.link} key={item.icon}>
+                        <a className={`material-icons-outlined ${router.pathname === item.link ? styles.active : ''}`}>
+                            {item.icon}
+                        </a>
+                    </Link>
+                ))}
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
